4dbb7b437f596220025de212a9bfa86a66774a5f
lianruby <rm95465@estudante.fieb.edu.br>
2025-03-14 09:38:42 -0300

    projeto2f

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+ class App extends Component{
+  render (){
+
+    return(
+      <div>
+        <h3>Aula React vite </h3>
+        <p>Retoma de conteudo</p>
+      </div>
+    )
+  }
+
+  
+ }
+ export default App  
